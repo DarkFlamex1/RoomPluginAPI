67d63652dda3cf312bce5e2445e19144bb0e0060
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2017-03-04 20:42:11 -0500

    Added code from other repo

---
+++ RoomPluginAPI/RoomMethods.cs
@@ -0,0 +1,38 @@
+﻿using DarkRift;
+using RoomPlugin;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoomMethods {
+
+    public void RoomCreate(string name, int MaxPlayers)
+    {
+        using (DarkRiftWriter write = new DarkRiftWriter())
+        {
+            write.Write(name);
+            write.Write(MaxPlayers);
+
+            DarkRiftAPI.SendMessageToServer(TagIndex.ROOM_CREATE, TagIndex.RoomSubjects.Room, write);
+        }
+    }
+
+    public void RoomJoin(string name)
+    {
+        using (DarkRiftWriter write = new DarkRiftWriter())
+        {
+            write.Write(name);
+
+            DarkRiftAPI.SendMessageToServer(TagIndex.ROOM_JOIN, TagIndex.RoomSubjects.Room, write);
+        }
+    }
+
+    public void RoomLeave(string name)
+    {
+        using (DarkRiftWriter write = new DarkRiftWriter())
+        {
+            write.Write(name);
+
+            DarkRiftAPI.SendMessageToServer(TagIndex.ROOM_LEAVE, TagIndex.RoomSubjects.Room, write);
+        }
+    }
+}
