e7c89b2803f1acd587ac3487df8dfc10d547e773
Vikram Peddinti <vikram.peddinti@gmail.com>
2017-03-04 19:11:00 -0800

    Adding some placeholder funtions

---
+++ RoomPluginAPI/Client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RoomPluginAPI
+{
+    public class Client
+    {
+        /// <summary>
+        /// Creates room with random generated name
+        /// </summary>
+        public void roomCreate()
+        {
+
+        }
+        /// <summary>
+        /// Create a room(Sends Message to server with name) 
+        /// Default Max Players is 10
+        /// </summary>
+        /// <param name="name"></param>
+        public void roomCreate(String name)
+        {
+
+        }
+
+        /// <summary>
+        /// Create a room(Sends Message to server with name and max players)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="max"></param>
+        public void roomCreate(String name, int max)
+        {
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
